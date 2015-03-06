7f1930e09ef77d21ec58caf1afced293cd2181db
Roger Herikstad <roger@admins-MacBook-Pro-2.local>
2015-03-06 11:19:21 +0800

    Moved type definition into separate file

--- src/Eyelink.jl
+++ src/Eyelink.jl
@@ -1,83 +1,9 @@
 module Eyelink
+include("types.jl")
 include("plot.jl")
 
 const _library = "/Library/Frameworks/edfapi.framework/Versions/Current/edfapi"
 
-datatypes = {0 => :nopending,
-			24 => :messageevent, 
-			 25 => :buttonevent,
-			 5 => :startsacc , 
-			 6 => :endsacc, 
-			 7 => :startfix, 
-			 8 => :endfix,
-			 28 => :inputevent,
-			 15 => :startsamples,
-			 16 => :endsamples,
-			 17 => :startevent,
-			 18	=> :endevent}
-
-
-type EDFFile
-	fname::String
-	ptr::Ptr{Void}
-	nevents::Int64
-	nsamples::Int64
-	nextevent::Symbol
-end
-
-function EDFFile(fname::String, ptr::Ptr)
-	EDFFile(fname,ptr,0,0,:unknown)
-end
-
-type EyeEvent
-	time::Int64
-	x::Float32
-	y::Float32
-end
-
-type LSTRING
-	len::Int16
-	c::Uint8
-end
-
-type FEVENT
-	time::Uint32
-	eventtype::Int16
-	read::Uint16
-	sttime::Uint32
-	entime::Uint32
-	hstx::Float32
-	hsty::Float32
-	gstx::Float32
-	gsty::Float32
-	sta::Float32
-	henx::Float32
-	heny::Float32
-	genx::Float32
-	geny::Float32
-	ena::Float32
-	havx::Float32
-	havy::Float32
-	gavx::Float32
-	gavy::Float32
-	ava::Float32
-	avel::Float32
-	pvel::Float32
-	svel::Float32
-	evel::Float32
-	supdx::Float32
-	eupdx::Float32
-	supdy::Float32
-	eupdy::Float32
-
-	eye::Int16
-	status::Uint16
-	flags::Uint16
-	input::Uint16
-	buttons::Uint16
-	parsedby::Uint16
-	message::Ptr{LSTRING}
-end
 
 function version()
 	_version = ccall((:edf_get_version, _library), Ptr{Uint8}, ())
