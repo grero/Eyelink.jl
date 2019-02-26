f92540908dcd1f4fc20fb92fcabccd03ffe22867
roger <roger.herikstad@gmail.com>
2019-02-26 17:45:53 +0800

    Slight refactoring to work with messages directly

--- src/calib.jl
+++ src/calib.jl
@@ -1,20 +1,25 @@
 """
 Return the validation points and errors.
 """
-function get_validation(data::EyelinkData)
-    val_msg = filter(ee->occursin("VALIDATE", ee.message), data.events)
+function get_validation(data::Eyelink.EyelinkData)
+    val_events = filter(ee->occursin("VALIDATE", ee.message), data.events)
+    messages = [ee.message for ee in val_events]
+    get_validation(messages)
+end
 
+function get_validation(messages::Vector{String})
+    _messages = filter(mm->occursin("VALIDATE", mm), messages)
     rpos = r"at ([0-9]*),([0-9]*)"
     rpix = r"([0-9.\-]*),([0-9.\-]*) pix"
-    cal_pos = fill(0.0, 2, length(val_msg))
-    cal_err = fill(0.0, 2, length(val_msg))
-    for (ii,mm) in enumerate(val_msg)
-        mpos = match(rpos, mm.message)
+    cal_pos = fill(0.0, 2, length(_messages))
+    cal_err = fill(0.0, 2, length(_messages))
+    for (ii,mm) in enumerate(_messages)
+        mpos = match(rpos, mm)
         if mpos.match != nothing
             cal_pos[1,ii] = parse(Float64, mpos[1])
             cal_pos[2,ii] = parse(Float64, mpos[2])
         end
-        mpix = match(rpix, mm.message)
+        mpix = match(rpix, mm)
         if mpix != nothing
             cal_err[1,ii] = parse(Float64, mpix[1])
             cal_err[2,ii] = parse(Float64, mpix[2])
