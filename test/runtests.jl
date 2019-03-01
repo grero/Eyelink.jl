ac4e5b8d7c15fe31277806d8ac6c0e207b657afb
roger <roger.herikstad@gmail.com>
2019-03-01 11:07:53 +0800

    Adds tests for edf loading

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,3 +10,22 @@ using Test
     @test cal_err[1,:] ≈ [1.4, -12.1,  -1.0, 17.7, -7.5,  2.9, -15.1, -19.6, -14.5]
     @test cal_err[2,:] ≈ [ -15.3, -3.6, -20.3, 3.7, 23.5, -0.2,  3.0, -15.0,  0.5]
 end
+
+@testset "Load file" begin
+    download("http://cortex.nus.edu.sg/testdata/w7_10_2.edf", "w7_10_2.edf")
+    eyelinkdata = Eyelink.load("w7_10_2.edf")
+    ll = length(eyelinkdata.events)
+    @show ll
+    @test ll == 32
+    sttime = [ee.sttime for ee in eyelinkdata.events[1:30]]
+    hh = hash(sttime)
+    @test hh == 0x963b31f9fdfdb0fa
+    mm = eyelinkdata.events[1].message
+    @test mm == "DISPLAY_COORDS 0 0 1920 1200"
+    tt = eyelinkdata.events[30].eventtype
+    @test tt == :inputevent
+    gx = eyelinkdata.samples.gx[2,1]
+    @test gx ≈ 846.2f0
+    gy = eyelinkdata.samples.gy[2,1]
+    @test gy ≈ 643.5f0
+end
